e02b0caf9929d809273a6dab781610b5b3d80e1a
Ross Timson <ross@rosstimson.com>
2012-12-09 18:30:28 +0000

    Spectrwm tweaks for taking screenshots.
    Adding script for taking screenshots via spectrwm default keybindings
    but saving into ~/Pictures/wallpapers instead of $HOME.
    
    Adding spectrwm quirk to float feh.
    
    Modifying spectrwm lock program.
    
    Changing font for spectrwm top bar.

---
+++ bin/screenshot.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+
+screenshot() {
+	case $1 in
+	full)
+		scrot -m -e 'mv $f ~/Pictures/screenshots/'
+		;;
+	window)
+		sleep 1
+		scrot -s -e 'mv $f ~/Pictures/screenshots/'
+		;;
+	*)
+		;;
+	esac;
+}
+
+screenshot $1
