6dd8e7a7594d8b346e2ac95fa699f57b9ba92bdd
Ross Timson <ross@rosstimson.com>
2016-06-01 22:01:32 +0100

    Initial commit where emacs is now usable (for me).
    Some very first settings for interface and enabling Evil (Vim mode).

---
+++ .emacs.d/init.el
@@ -0,0 +1,106 @@
+;;; init.el --- Emacs configuration of Ross Timson -*- lexical-binding: t; -*-
+;;
+;; Copyright (c) 2016-2016 Ross Timson <ross@rosstimson.com>
+;;
+;; Author: Ross Timson <ross@rosstimson.com>
+;; URL: https://gihub.com/rosstimson/dotfiles
+;; Keywords: convenience
+
+;; This file is not part of GNU Emacs.
+
+;; Copyright (c) 2016, Ross Timson <ross@rosstimson>
+
+;; Permission to use, copy, modify, and/or distribute this software for
+;; any purpose with or without fee is hereby granted, provided that the
+;; above copyright notice and this permission notice appear in all copies.
+
+;; THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+;; WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
+;; OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE
+;; FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
+;; DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+;; IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+;; OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+;;; Commentary:
+
+;; Emacs configuration of Ross Timson.
+
+;;; Code:
+
+
+;;; Package Management
+
+;; Always load newest byte code
+(setq load-prefer-newer t)
+
+(require 'package)
+(setq package-enable-at-startup nil)
+
+;;; Standard package repositories
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+
+
+;;; Essential settings
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+;; Reduce the frequency of garbage collection by making it happen on
+;; each 50MB of allocated data (the default is on every 0.76MB)
+(setq gc-cons-threshold 50000000)
+
+;; Warn when opening files bigger than 100MB
+(setq large-file-warning-threshold 100000000)
+
+;; User interface
+(setq inhibit-splash-screen t
+      inhibit-startup-message t
+      inihibt-startup-echo-area-message t)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(show-paren-mode 1)
+(blink-cursor-mode -1)
+(global-hl-line-mode t)
+
+;; Short Yes/No questions
+(fset 'yes-or-no-p #'y-or-n-p)
+
+;; Colour theme
+(use-package color-theme-sanityinc-tomorrow
+  :ensure t
+  :init (load-theme 'sanityinc-tomorrow-night 'no-confirm))
+
+
+
+;;; Packages
+
+(use-package evil
+  :ensure t
+  :config (evil-mode 1))
+
+(use-package smex
+  :ensure t
+  :bind (("M-x" . smex)
+         ("M-X" . smex-major-mode-commands)))
+
+
+;; Local Variables:
+;; coding: utf-8
+;; indent-tabs-mode: nil
+;; End:
+
+;;; init.el ends here
