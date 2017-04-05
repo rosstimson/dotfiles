66efc5c21bb5b90c61ebb9ae1bbeb3d9f53b4060
Ross Timson <ross@rosstimson.com>
2017-04-05 20:15:10 +0100

    Hopefully fixing zsh search through history.
    Search throught history with cursor keys.

--- .zsh/lib/key-bindings.zsh
+++ .zsh/lib/key-bindings.zsh
@@ -6,8 +6,8 @@ bindkey "^[[5~" up-line-or-history
 bindkey "^[[6~" down-line-or-history
 
 # make search up and down work, so partially type and hit up/down to find relevant stuff
-bindkey '^[[A' up-line-or-search
-bindkey '^[[B' down-line-or-search
+bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
 
 bindkey "^[[H" beginning-of-line
 bindkey "^[[1~" beginning-of-line
