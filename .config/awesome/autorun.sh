f615faf874ca20cc045c35260db7cbfa7c74a8bd
Ross Timson <ross@rosstimson.com>
2018-07-05 21:26:29 +0100

    Autostart things with AwesomeWM.
    On Fedora since it is using Gnome and friends inc. GDM the .xinitrc
    doesn't seem to be respected so start stuff I'd normally start via
    .xinitrc using Awesome's own method.  The shell script _should_ check to
    make sure if a process is running already so this hopefully won't
    interfere when used on more minimal systems where the .xinitrc is being
    respected such as BSD and Voiid.

---
+++ .config/awesome/autorun.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Helper function to determine if proces is already running before
+# starting a new one.
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+# These don't have long running processes so don't need to be run via
+# the 'run' helper function.
+setxkbmap -model pc104 -layout us -option -option ctrl:nocaps
+
+# Start long running processes using the helper function to ensure we
+# don't get duplicates.
+run urxvtd -q -o -f
+run ssh-agent
+run gpg-agent --daemon --allow-emacs-pinentry
+run emacs --daemon
