4af97ed4bfeb02320ed73f8f2469a87c65f7e081
Ross Timson <ross@rosstimson.com>
2018-07-12 19:07:37 +0100

    Remove Marmalade Emacs package repo.
    SSL cert has expired and everything seems to be available on MELPA
    anyway.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -42,7 +42,6 @@
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 (package-initialize)
 
