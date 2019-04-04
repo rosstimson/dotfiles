791db9e032dc67bc8a913d34241fccb5cfd57571
Ross Timson <me@rosstimson.com>
2019-04-04 18:28:12 +0100

    Config touchpad & keyboard in Awesome WM.

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -12,9 +12,13 @@ function run {
 # These don't have long running processes so don't need to be run via
 # the 'run' helper function.
 #
-# Set a standard PC keyboard with US layout, and caps-locks is another
+# Set a standard PC keyboard with Colemak layout, and caps-locks is another
 # control key.
-setxkbmap -model pc104 -layout "us" -option ctrl:nocaps
+setxkbmap us -variant colemak -option ctrl:nocaps
+
+# Set touchpad options for tap to click and natural scrolling.
+xinput set-prop 'SynPS/2 Synaptics TouchPad' 'libinput Tapping Enabled' 1
+xinput set-prop 'SynPS/2 Synaptics TouchPad' 'libinput Natural Scrolling Enabled' 1
 
 # Start long running processes using the helper function to ensure we
 # don't get duplicates.
