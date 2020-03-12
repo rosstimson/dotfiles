69eb5303b2b343ebe4484ad08c2097f457212858
Ross Timson <ross.timson@matillion.com>
2020-03-12 22:00:57 +0000

    Initial global eslint config
    Global configs are deprecated but I can use this one as a template to
    copy into projects or use the 'eslint --config' flag.

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+	"env": {
+		"node": true,
+		"es6": true
+	},
+	"extends": "eslint:recommended",
+	"rules": {
+		"eqeqeq": "error",
+		"indent": [
+			"error",
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"no-console": "off",
+		"no-var": "error",
+		"prefer-const": "error",
+		"quotes": [
+			"error",
+			"single",
+			"avoid-escape"
+		],
+		"semi": [
+			"error",
+			"always"
+		],
+		"strict": "error",
+	}
+};
