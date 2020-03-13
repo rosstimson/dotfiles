50cbfe99a79d45a56f5052f5eca63b3d5b87017a
Ross Timson <me@rosstimson.com>
2020-03-13 16:57:53 +0000

    Add more rules to eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,25 @@
 module.exports = {
-	"env": {
-		"node": true,
-		"es6": true
+	'env': {
+		'node': true,
+		'es6': true,
 	},
-	"extends": "eslint:recommended",
-	"rules": {
-		"eqeqeq": "error",
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"no-console": "off",
-		"no-var": "error",
-		"prefer-const": "error",
-		"quotes": [
-			"error",
-			"single",
-			"avoid-escape"
-		],
-		"semi": [
-			"error",
-			"always"
-		],
-		"strict": "error",
-	}
+	'extends': 'eslint:recommended',
+	'rules': {
+		'brace-style': 'error',
+		'camelcase': 'error',
+		'comma-dangle': ['error', 'always-multiline'],
+		'comma-spacing': 'error',
+		'comma-style': 'error',
+		'curly': 'error',
+		'eqeqeq': 'error',
+		'indent': ['error',	'tab'],
+		'linebreak-style': ['error', 'unix'],
+		'no-var': 'error',
+		'prefer-const': 'error',
+		'quotes': ['error',	'single', 'avoid-escape'],
+		'semi': ['error', 'always'],
+		'semi-spacing': 'error',
+		'semi-style': ['error', 'last'],
+		'strict': 'error',
+	},
 };
