127840ceb2ca11e2363f4cf122a870324898fc64
Ross Timson <me@rosstimson.com>
2022-07-23 15:57:00 +0100

    feat(emacs): add modus theme & make default

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,15 +3,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("4a288765be220b99defaaeb4c915ed783a9916e3e08f33278bf5ff56e49cbc73" "5a611788d47c1deec31494eb2bb864fde402b32b139fe461312589a9f28835db" "a5944520983c5467f163f68d256ff869a338c88acbd3610ffde4ba259c51dc61" default))
  '(epa-file-name-regexp "\\.\\(gpg\\|asc\\)$")
  '(initial-frame-alist '((fullscreen . maximized)))
  '(magit-commit-arguments '("--verbose" "--gpg-sign=667A3481E4BB34F3"))
- '(safe-local-variable-values '((eval auto-fill-mode t))))
+ '(safe-local-variable-values '((eval auto-fill-mode t)))
+ '(warning-suppress-log-types '((modus-themes))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:height 140 :family "Dank Mono"))))
  '(aw-leading-char-face ((t (:foreground "red" :height 3.0))))
- '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :extend nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Dank Mono"))))
  '(variable-pitch ((t (:height 1.0 :family "Linux Libertine Display O")))))
