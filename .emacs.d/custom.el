09440d4b5725a51983aa95b5e1d1264eed907a69
Ross Timson <me@rosstimson.com>
2023-05-04 21:31:57 +0100

    feat(emacs): set cartograph as default font
    Just comment out Dank Mono so I can easily switch between the two
    without having to remember the names when using M-x customize-face.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -15,6 +15,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 140 :family "Dank Mono"))))
+ '(default ((t (:height 140 :family "Cartograph CF"))))
+ ;;'(default ((t (:height 140 :family "Dank Mono"))))
  '(variable-pitch ((t (:height 1.0 :family "ETBembo")))))
 
