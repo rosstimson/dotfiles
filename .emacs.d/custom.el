2abe75972737757cca5738eb9fea8d6e69c8e35a
Ross Timson <me@rosstimson.com>
2024-02-13 16:39:53 +0000

    refactor(emacs): put default frame size & scroll bar into customize
    By putting these settings into customize they seem more reliable and
    consistent when also using emacsclient.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,10 +5,13 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("4a288765be220b99defaaeb4c915ed783a9916e3e08f33278bf5ff56e49cbc73" "5a611788d47c1deec31494eb2bb864fde402b32b139fe461312589a9f28835db" "a5944520983c5467f163f68d256ff869a338c88acbd3610ffde4ba259c51dc61" default))
+ '(default-frame-alist '((fullscreen . maximized)))
  '(epa-file-name-regexp "\\.\\(gpg\\|asc\\)$")
+ '(horizontal-scroll-bar-mode nil)
  '(initial-frame-alist '((fullscreen . maximized)))
  '(magit-commit-arguments '("--verbose" "--gpg-sign=667A3481E4BB34F3"))
  '(safe-local-variable-values '((eval auto-fill-mode t)))
+ '(scroll-bar-mode nil)
  '(warning-suppress-log-types '((modus-themes))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -16,6 +19,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:height 140 :family "Cartograph CF"))))
- ;;'(default ((t (:height 140 :family "Dank Mono"))))
  '(variable-pitch ((t (:height 1.0 :family "ETBembo")))))
 
