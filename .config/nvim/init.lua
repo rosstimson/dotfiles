fea204f1ae96be82dc95b57c54979f8ee29204be
Ross Timson <me@rosstimson.com>
2025-10-30 21:16:14 +0000

    feat(nvim): enable indent_line & lint

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -974,8 +974,8 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
