57803244a92b64e022ad6814dab1ec26cd6e37b6
Ross Timson <me@rosstimson.com>
2025-11-13 17:07:18 +0000

    refactor(nvim): set correct mini.nvim repo

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -818,7 +818,7 @@ require('lazy').setup({
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
   { -- Collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
+    'nvim-mini/mini.nvim',
     config = function()
       -- Better Around/Inside textobjects
       --
