c17e16aaffc85f86bd11a5a5a7c51b881642c92e
Ross Timson <me@rosstimson.com>
2025-11-13 17:19:22 +0000

    feat(nvim): initial install of avante

---
+++ .config/nvim/lua/custom/plugins/avante.lua
@@ -0,0 +1,16 @@
+return {
+  'yetone/avante.nvim',
+  event = 'VeryLazy',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'nvim-telescope/telescope.nvim',
+  },
+  opts = {
+    -- Start in manual mode; you can switch to "agentic" later
+    mode = 'legacy',
+  },
+}
+
+
