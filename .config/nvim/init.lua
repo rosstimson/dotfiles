22a4432734821e9b273b020acac2984b4782f616
Ross Timson <me@rosstimson.com>
2025-11-14 17:47:06 +0000

    feat(nvim): add percentage through page in status bar

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -859,10 +859,10 @@ require('lazy').setup({
 
       -- You can configure sections in the statusline by overriding their
       -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
+      -- cursor location to LINE:COLUMN PERCENTAGE
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
-        return '%2l:%-2v'
+        return '%2l:%-2v%p%%'
       end
 
       -- ... and there is more!
