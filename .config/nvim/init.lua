98db9d858e82052c54b33212f0285b82dcfaaea2
Ross Timson <me@rosstimson.com>
2025-11-14 17:47:40 +0000

    feat(nvim): add catppuccin colours

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -916,6 +916,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   { import = 'custom.plugins' },
+  { import = 'custom.colourschemes' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
