2b3a5b444ade6a50608734a454b989314706ddb9
Ross Timson <me@rosstimson.com>
2026-01-12 15:26:31 +0000

    refactor(neovim): acp config for opencode from docs

--- .config/nvim/lua/custom/plugins/codecompanion.lua
+++ .config/nvim/lua/custom/plugins/codecompanion.lua
@@ -1,27 +1,12 @@
 return {
   "olimorris/codecompanion.nvim",
   opts = {
-    adapters = {
-      acp = {
-        gemini_cli = function()
-          return require("codecompanion.adapters").extend("gemini_cli", {
-            defaults = {
-              auth_method = "gemini-api-key", -- "oauth-personal"|"gemini-api-key"|"vertex-ai"
-            },
-            env = {
-              GEMINI_API_KEY = "cmd:op read op://personal/Gemini_API/credential --no-newline",
-            },
-          })
-        end,
-        opencode = function()
-          return require("codecompanion.adapters").extend("opencode", {
-            commands = {
-              default = {
-                "opencode",
-              },
-            },
-          })
-        end,
+    strategies = {
+      chat = {
+        adapter = {
+          name = "opencode",
+          model = "openai/gpt-5-codex",
+        },
       },
     },
   },
