a3ffbc57e63b7a405443e13dede583e1b6ed3f03
Ross Timson <me@rosstimson.com>
2026-01-16 22:33:36 +0000

    refactor(nvim): set super-tab preset for completions

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -782,7 +782,7 @@ require('lazy').setup({
         -- <c-k>: Toggle signature help
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'default',
+        preset = 'super-tab',
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
